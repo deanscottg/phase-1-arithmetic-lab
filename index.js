a401e3345c9452c8af2b5dde70b02674d01a25a8
Dean <deanscottg@gmail.com>
2022-01-31 13:28:14 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random(0,10) * 10) + 1;
+
+const num3 = 54
+const num4 = 10
+let mod = num3 % num4;
+
+let max = Math.max(1,2,5,3,20);
